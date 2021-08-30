1c566868c0ab24b7724671b4c3984889dcf0956a
dwikinuridhuha <dwiki.nuridhuha@gmail.com>
2021-08-30 08:17:52 +0700

    deploy

---
+++ precache-manifest.0aca27c6c435297be1a4f961dbc1ea78.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc151b484d130c1412dd71136d88c337",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d2be6a38d87f4816944",
+    "url": "/static/css/main.44b9f4c2.chunk.css"
+  },
+  {
+    "revision": "66821660d585f386325f",
+    "url": "/static/js/2.aa2b3d06.chunk.js"
+  },
+  {
+    "revision": "baebdd1671010f55d6a3b55cb359d81e",
+    "url": "/static/js/2.aa2b3d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d2be6a38d87f4816944",
+    "url": "/static/js/main.9f053a52.chunk.js"
+  },
+  {
+    "revision": "aea68ea76404d8ab4da2",
+    "url": "/static/js/runtime-main.699a0f81.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "d6c4e78d7090519fda9f11d208356f79",
+    "url": "/static/media/up-arrow-circle.d6c4e78d.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
